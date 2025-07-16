a538c2b1ee642d2b89d755ea9bf701db84333ad0
beingvinod <vinuvinod786@gmail.com>
2025-07-16 23:07:39 +0530

    Create App.java

---
+++ src/main/java/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from Jenkins and SonarQube!");
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
